d9c884c65d5fffab896cc4c03316e44a8c7b5ffe
damoreluc <damore.luca@isisbassafriulana.edu.it>
2023-03-14 17:45:53 +0100

    base

--- src/main.cpp
+++ src/main.cpp
@@ -2,74 +2,87 @@
 #include <userdatatypes/sampletype.h>
 #include <acquisition/sampler.h>
 
+// ISR su APP_CPU (1)
+// trasmissione seriale su PRO_CPU(0)
+// coda per il passaggio dei dati tra i due task
+
+// dimensione della coda
+static const uint8_t msgQueueLen = 20;
+// coda di comunicazione tra task, variabile globale
+static QueueHandle_t msg_queue;
+
 // impiego di un timer hardware per il periodo di campionamento
 // periodo di campionamento Ts = 200us
+uint32_t Ts_us = 200;
 
 // puntatore alla struttura timer da associare al timer hardware
 hw_timer_t *timer = NULL;
-// dataReady è posto a true nella ISR dopo aver acquisito tutti i campioni del buffer
-volatile bool dataReady = false;
 // dichiarazione della interrupt handling routine
 void IRAM_ATTR onTimer();
-
-// dimensione del buffer dati
-const u_int32_t BUFFER_LENGTH = 5000;
-
-// buffer dei campioni
-volatile Sample sampleBuffer[BUFFER_LENGTH];
-// puntatore alla prossima cella libera del buffer
-volatile Sample *pSampleBuffer = &sampleBuffer[0];
 
 // indice nel buffer dati
 volatile uint32_t indice = 0;
 
+// Implementazione dei task
+// task che attende un item nella coda e lo stampa
+void printTask(void *parameters)
+{
+  // variabile locale col dato prelevato dalla coda
+  Sample item;
+
+  // stampa sul terminale
+  while (1)
+  {
+    // controlla se è presente un messaggio nella coda
+    // e in caso affermativo lo preleva (non bloccante)
+    if (xQueueReceive(msg_queue, (void *)&item, 2) == pdTRUE)
+    {
+      Serial.printf("%4d\t%4d\t%4d\t\t%3d\n", item.iax, item.iay, item.iaz, uxQueueSpacesAvailable(msg_queue));
+    }
+  }
+}
+
 void setup()
 {
   Serial.begin(921600);
-  Serial.printf("Sampler %d campioni con ISR\n", BUFFER_LENGTH);
+  Serial.printf("Sampler %d campioni con ISR e coda\n", msgQueueLen);
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+  // creazione della coda
+  msg_queue = xQueueCreate(msgQueueLen, sizeof(Sample));
+
+  // creazione ed avvio del task di stampa
+  xTaskCreatePinnedToCore(
+      printTask,    // funzione da richiamare nel task
+      "Print Task", // nome del task (etichetta utile per debug)
+      2000,         // dimensione in byte dello stack per le variabili locali del task (minimo 768 byte)
+      NULL,         // puntatore agli eventuali parametri da passare al task
+      1,            // priorità del task
+      NULL,         // eventuale task handle per gestire il task da un altro task
+      PRO_CPU_NUM   // core su cui far girare il task
+  );
 
   // impiego il timer hardware numero 0, con prescaler 80 (Tbit = 1us), conteggio in avanti
   timer = timerBegin(0, 80, true);
   // associazione della ISR al timer per gestire l'interrupt periodico
   timerAttachInterrupt(timer, onTimer, true);
-  // impostazione del periodo di campionamento: 200us
-  timerAlarmWrite(timer, 200, true);
+  // impostazione del periodo di campionamento: 100us
+  timerAlarmWrite(timer, Ts_us, true);
   // abilitazione del timer
   timerAlarmEnable(timer);
 }
 
 void loop()
 {
-
-  // quando il buffer è pieno...
-  if (dataReady)
-  {
-    // stampa dei dati
-    for (indice = 0; indice < BUFFER_LENGTH; indice++)
-    {
-      Serial.printf("%4d\t%4d\t%4d\n", sampleBuffer[indice].iax, sampleBuffer[indice].iay, sampleBuffer[indice].iaz);
-    }
-    Serial.println();
-
-    delay(10);
-    // rewind del puntatore e dell'indice
-    pSampleBuffer = &sampleBuffer[0];
-    indice = 0;
-    dataReady = false;
-  }
 }
 
 // definition of the interrupt handling routine
 void IRAM_ATTR onTimer()
 {
-  if (indice < BUFFER_LENGTH && !dataReady)
-  {
-    getSample(pSampleBuffer++);
-    indice++;
-  }
+  Sample item;
+  BaseType_t result;
 
-  if (indice >= BUFFER_LENGTH)
-  {
-    dataReady = true;
-  }
+  getSample(&item);
+  result = xQueueSendFromISR(msg_queue, (void*)&item, NULL);
+
 }
